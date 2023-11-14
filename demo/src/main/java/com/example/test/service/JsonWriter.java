b79ef46b4fea5a97ab809bdfa4f77f1bac13c1c1
Skopin <gospodin58@yandex.ru>
2023-11-14 12:20:20 +0300

    use @Slf4j

--- demo/src/main/java/com/example/test/service/JsonWriter.java
+++ demo/src/main/java/com/example/test/service/JsonWriter.java
@@ -8,25 +8,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.example.test.utils.LocalDateAdapter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class JsonWriter {
     
     private Path fullPath;
-    private Logger logger;
 
     public JsonWriter(String outputPath, String fileName){
         this.fullPath = Paths.get(outputPath, fileName);
-        this.logger = LoggerFactory.getLogger(JsonWriter.class);
         try{
             Files.createDirectories(Paths.get(outputPath));
         } catch (IOException exception){
-            this.logger.error(exception.getMessage());
+            log.error(exception.getMessage());
         }
     }
 
@@ -39,7 +37,7 @@ public class JsonWriter {
                 .create();
             gson.toJson(data, writer);
         } catch (IOException exception) {
-            this.logger.error(exception.getMessage());
+            log.error(exception.getMessage());
         }
     }
 
