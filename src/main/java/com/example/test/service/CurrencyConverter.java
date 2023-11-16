5af77e86142d22ab42a81cfc3363414d6bd471c2
Skopin <gospodin58@yandex.ru>
2023-11-16 22:54:25 +0300

    testsForCurrencyConverter

--- src/main/java/com/example/test/service/CurrencyConverter.java
+++ src/main/java/com/example/test/service/CurrencyConverter.java
@@ -5,6 +5,8 @@ import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.LoggerFactory;
+
 import com.example.test.enums.Currency;
 
 import lombok.val;
@@ -17,13 +19,14 @@ public class CurrencyConverter {
     }};
 
     public static void convertAll(Map<Currency, BigDecimal> prices){
+        val logger = LoggerFactory.getLogger(XHTMLParser.class);
         // Если нет цены в USD, то находим первую валюту и конвертируем ее в USD
         if (!prices.containsKey(Currency.USD)){
             for (val entry : prices.entrySet()) {
                 val entryCurrency = entry.getKey();
                 val entryPrice = entry.getValue();
-                val usdPrice = entryPrice.divide(USD2CurrencyRates.get(entryCurrency)); 
-                prices.put(entryCurrency, usdPrice);
+                val usdPrice = entryPrice.divide(USD2CurrencyRates.get(entryCurrency)).setScale(2, RoundingMode.HALF_DOWN); 
+                prices.put(Currency.USD, usdPrice);
                 break;
             }
         }
@@ -33,6 +36,7 @@ public class CurrencyConverter {
             if (currency == Currency.USD){
                 continue;
             }
+            
             val newPrice = usdPrice.multiply(
                 USD2CurrencyRates.get(currency))
                 .setScale(2, RoundingMode.HALF_DOWN
